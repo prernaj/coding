6c199ec02c486f9ef6185a2ee3afeef1e26ebfb0
Prerna Jeslani <prernajeslani@gmail.com>
2017-09-09 22:10:20 +0530

    Adding liked list programs

---
+++ ds/linked_list/Singly_linked_list/deletion_given_key.py
@@ -0,0 +1,100 @@
+'''
+given a key, delete the first occurence of this key in linked list.
+to delete a node from linked list, we need to do following steps:
+1. find previous node of the node to be deleted.
+2. change next of previous node
+3. free memory for the node to be deleted
+'''
+
+
+class Node():
+    
+    def __init__(self, data):
+        self._data = data
+        self._next = None
+
+    def get_data(self):
+        return self._data
+
+    def get_next(self):
+        return self._next
+
+    def set_next(self, node):
+        self._next = node
+
+
+class LinkedList():
+
+    def __init__(self):
+        self._head = None
+
+    def insert_front(self, data):
+        new_node = Node(data)
+        if self._head == None:
+            self._head = new_node
+            return self._head
+        temp = self._head
+        while temp and temp.get_next() is not None:
+            temp = temp.get_next()
+
+        temp.set_next(new_node)
+        return self._head
+
+    def insert_after(self, data, node):
+        new_node = Node(data)
+        if node is not None:
+            node_next = node.get_next()
+            node.set_next(new_node)
+            new_node.set_next(node_next)
+
+
+    def insert_end(self, data):
+        new_node = Node(data)
+        temp = self._head
+        while temp and temp.get_next() is not None:
+            temp = temp.get_next()
+        temp.set_next(new_node)
+
+    def get_head(self):
+        return self._head
+
+    def print_ll(self):
+        temp = self._head
+        while temp:
+            print temp.get_data(),
+            temp = temp.get_next()
+
+    def delete_key(self, item):
+        temp = self._head
+        while temp:
+            if temp.get_next() == None:
+                print 'Cannot find key'
+                return
+            if temp.get_next().get_data() == item:
+                elem = temp.get_next()
+                temp.set_next(elem.get_next())
+                print 'Deleting', elem.get_data()
+                del elem
+                return
+            temp = temp.get_next()
+    
+
+def main():
+    ll = LinkedList()
+    ll.insert_front(1)
+    ll.insert_front(2)
+    h = ll.get_head()
+    if h:
+        ll.insert_after(3, h.get_next())
+    ll.insert_end(4)
+    ll.print_ll()
+    ll.delete_key(3)
+    ll.print_ll()
+
+if __name__=='__main__':
+    main()
+
+
+
+
+
---
+++ ds/linked_list/Singly_linked_list/search_elem.py
@@ -0,0 +1,90 @@
+'''
+'''
+
+
+class Node():
+    
+    def __init__(self, data):
+        self._data = data
+        self._next = None
+
+    def get_data(self):
+        return self._data
+
+    def get_next(self):
+        return self._next
+
+    def set_next(self, node):
+        self._next = node
+
+
+class LinkedList():
+
+    def __init__(self):
+        self._head = None
+
+    def insert_front(self, data):
+        new_node = Node(data)
+        if self._head == None:
+            self._head = new_node
+            return self._head
+        temp = self._head
+        while temp and temp.get_next() is not None:
+            temp = temp.get_next()
+
+        temp.set_next(new_node)
+        return self._head
+
+    def insert_after(self, data, node):
+        new_node = Node(data)
+        if node is not None:
+            node_next = node.get_next()
+            node.set_next(new_node)
+            new_node.set_next(node_next)
+
+
+    def insert_end(self, data):
+        new_node = Node(data)
+        temp = self._head
+        while temp and temp.get_next() is not None:
+            temp = temp.get_next()
+        temp.set_next(new_node)
+
+    def get_head(self):
+        return self._head
+
+    def print_ll(self):
+        temp = self._head
+        while temp:
+            print temp.get_data(),
+            temp = temp.get_next()
+
+    def search_elem(self, key):
+        temp = self._head
+        while temp:
+            if temp.get_data() == key:
+                return temp
+            temp = temp.get_next()
+        return None
+
+    
+
+def main():
+    ll = LinkedList()
+    ll.insert_front(1)
+    ll.insert_front(2)
+    h = ll.get_head()
+    if h:
+        ll.insert_after(3, h.get_next())
+    ll.insert_end(4)
+    ll.print_ll()
+    s_node = ll.search_elem(2)
+    print s_node.get_data()
+
+if __name__=='__main__':
+    main()
+
+
+
+
+
