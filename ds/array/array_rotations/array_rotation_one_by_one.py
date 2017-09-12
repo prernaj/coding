c30e5c8c7b523874e9b7c0a4e2d724da984d4b4d
Prerna Jeslani <prernajeslani@gmail.com>
2017-09-12 21:34:05 +0530

    improving code of array_rotation_one_by_one.py

--- ds/array/array_rotations/array_rotation_one_by_one.py
+++ ds/array/array_rotations/array_rotation_one_by_one.py
@@ -5,10 +5,13 @@ space O(1)
 '''
 def rotate_left(arr, n, d):
     for i in range(0,d):
-        first_elem = arr[0]
-        for j in range(0, n-1):
-            arr[j] = arr[j+1]
-        arr[n-1] = first_elem
+        rotate_left_by_one(arr, n)
+
+def rotate_left_by_one(arr, n):
+    first_elem = arr[0]
+    for j in range(0, n-1):
+        arr[j] = arr[j+1]
+    arr[n-1] = first_elem
 
 def main():
     arr = [1,2,3,4,5,6,7]
