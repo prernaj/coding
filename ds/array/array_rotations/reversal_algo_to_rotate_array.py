59c99a88915fa8be8662f5a9dada2f73e73e248d
Prerna Jeslani <prernajeslani@gmail.com>
2017-09-13 01:59:14 +0530

    improved reversal_algo_to_rotate_array.py

--- ds/array/array_rotations/reversal_algo_to_rotate_array.py
+++ ds/array/array_rotations/reversal_algo_to_rotate_array.py
@@ -3,23 +3,23 @@ BA = R(ArBr)
 A is arr(0:d)
 B is arr(d:n)
 '''
-def reverse_array(arr, n):
-    for i in range(0,n):
-        j = n-i-1
+def reverse_array(arr, a, b):
+    for i in range(a, b):
+        j = b-i-1
         if i > j:
             break
         arr[i], arr[j] = arr[j], arr[i]
+
+def rotate_left(arr, n, d):
+    reverse_array(arr, 0, d)
+    reverse_array(arr, d, n)
+    reverse_array(arr, 0, n)
 
 def main():
     arr = [1,2,3,4,5,6,7]
     n = 7
     d = 2
-    A = arr[0:d]
-    reverse_array(A, d)
-    B = arr[d:n]
-    reverse_array(B, n-d)
-    arr = A+B
-    reverse_array(arr, 7)
+    rotate_left(arr, n, d)
     for item in arr:
         print item,
 
